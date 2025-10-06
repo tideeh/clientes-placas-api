25be7b0305bd5c44403b9799348d413323043375
André Martins Pereira <andre_mp_@hotmail.com>
2025-10-06 17:20:49 -0300

    feat: normalizar e Number Magics

--- src/app/Http/Requests/StoreClienteRequest.php
+++ src/app/Http/Requests/StoreClienteRequest.php
@@ -2,37 +2,51 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Domain\Cliente\Policies\ClientePolicies as P;
 use App\Http\Responses\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreClienteRequest extends FormRequest
 {
-	public function authorize(): bool
-	{
-		return true;
-	}
+    public function authorize(): bool
+    {
+        return true;
+    }
 
-	protected function failedValidation(Validator $validator)
-	{
-		throw new HttpResponseException(
-			ApiResponse::error('Erro de validação', 422, $validator->errors())
-		);
-	}
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            ApiResponse::error('Erro de validação', 422, $validator->errors())
+        );
+    }
 
-	public function rules(): array
-	{
-		return [
-			'nome' => ['required', 'string', 'max:120'],
-			'telefone' => ['required', 'string', 'max:20', 'regex:/^[0-9()+ \-]+$/'],
-			'cpf'  => ['required', 'digits:11', 'unique:clientes,cpf'],
-			'placa_carro' => [
-				'required',
-				'string',
-				'max:7',
-				'regex:/^([A-Z]{3}[0-9]{4}|[A-Z]{3}[0-9][A-Z][0-9]{2})$/'
-			],
-		];
-	}
+    protected function prepareForValidation(): void
+    {
+        $cpf = preg_replace('/\D/', '', (string) $this->input('cpf'));
+        $tel = preg_replace('/\D/', '', (string) $this->input('telefone'));
+        $placa = strtoupper((string) $this->input('placa_carro'));
+
+        $this->merge([
+            'cpf' => $cpf,
+            'telefone' => $tel,
+            'placa_carro' => $placa,
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nome' => ['required', 'string', 'max:120'],
+            'telefone' => ['required', 'string', 'max:20'],
+            'cpf' => ['required', 'digits:'.P::CPF_DIGITS, 'unique:clientes,cpf'],
+            'placa_carro' => [
+                'required',
+                'string',
+                'size:'.P::PLACA_SIZE,
+                'regex:'.P::PLACA_REGEX,
+            ],
+        ];
+    }
 }
--- src/app/Http/Requests/UpdateClienteRequest.php
+++ src/app/Http/Requests/UpdateClienteRequest.php
@@ -3,17 +3,46 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class UpdateClienteRequest extends FormRequest {
-  public function authorize(): bool { return true; }
-  public function rules(): array {
-    $id = $this->route('id');
-    return [
-      'nome' => ['sometimes','string','max:120'],
-      'telefone'=> ['sometimes','string','max:20','regex:/^[0-9()+ \-]+$/'],
-      'cpf'  => ['sometimes','digits:11',"unique:clientes,cpf,{$id}"],
-      'placa_carro' => ['sometimes','string','max:7',
-        'regex:/^([A-Z]{3}[0-9]{4}|[A-Z]{3}[0-9][A-Z][0-9]{2})$/'],
-    ];
-  }
+class UpdateClienteRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $cpf = preg_replace('/\D/', '', (string) $this->input('cpf'));
+        $tel = preg_replace('/\D/', '', (string) $this->input('telefone'));
+        $placa = strtoupper((string) $this->input('placa_carro'));
+
+        $this->merge([
+            'cpf' => $cpf,
+            'telefone' => $tel,
+            'placa_carro' => $placa,
+        ]);
+    }
+
+    public function rules(): array
+    {
+        $id = (int) $this->route('id');
+
+        return [
+            'nome' => ['sometimes', 'string', 'max:120'],
+            'telefone' => ['sometimes', 'string', 'max:20'],
+            'cpf' => [
+                'sometimes',
+                'digits:11',
+                Rule::unique('clientes', 'cpf')->ignore($id),
+            ],
+            'placa_carro' => [
+                'sometimes',
+                'string',
+                'size:7',
+                'regex:/^(?:[A-Z]{3}\d{4}|[A-Z]{3}\d[A-Z]\d{2})$/',
+            ],
+        ];
+    }
 }
